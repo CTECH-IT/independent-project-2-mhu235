f78ce0b197c0f6a79c9359d7a68dd1492aed605a
Maggie Hu <8508601@isd535.org>
2022-03-06 23:07:25 -0600

    thumbnails work, deleted onward

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,60 @@
-// Put your JavaScript here
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_DESCRIPTION_SELECTOR = '[data-image-role="description"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imageUrl, titleText, descriptionText) {
+    'use strict';
+    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+
+    let description = document.querySelector(DETAIL_DESCRIPTION_SELECTOR);
+    description.textContent = descriptionText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function descriptionFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-description');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail),
+    descriptionFromThumb(thumbnail));
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    let thumbnailArray = [].slice.call(thumbnails);
+    return thumbnailArray;
+}
+
+function addThumbClickHandler(thumb) {
+    'use strict';
+    thumb.addEventListener('click', function(event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+    });
+}
+
+function initializeEvents() {
+    'use strict';
+    let thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+}
+
+initializeEvents();
